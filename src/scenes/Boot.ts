630a11f175f624e51fb31e5115af343f36c3c01d
beatriz-20 <gabriela.b2006@aluno.ifsc.edu.br>
2025-05-21 18:23:38 +0000

    adicionar segunda cena

---
+++ src/scenes/Boot.ts
@@ -0,0 +1,17 @@
+import { Scene } from "phaser";
+
+export class Boot extends Scene {
+  constructor() {
+    super({ key: "Boot" });
+  }
+
+  preload() {
+    this.load.image("corridor", "assets/corridor.png");
+  }
+
+  create() {
+      this.add
+          .image(400, 225, "corridor");
+      
+  }
+}
